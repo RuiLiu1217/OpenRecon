e5ffa59cb1d103e9b41779c136ea4c5723315efa
Rui Liu <liurui1217@gmail.com>
2021-01-31 16:48:42 -0500

    update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/liuru/Dropbox/MyWebsite/OpenRecon
+# Build directory: C:/Users/liuru/Dropbox/MyWebsite/OpenRecon/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
+subdirs("test")

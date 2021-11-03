a608aa682a6098f9b360b2e225f0eab3acc247d4
Rui Liu <liurui1217@gmail.com>
2021-11-03 00:19:35 -0400

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/liuru/OneDrive/Research/NewProject/OpenRecon
-# Build directory: C:/Users/liuru/OneDrive/Research/NewProject/OpenRecon/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/liuru/OneDrive/Research/NewProject/OpenRecon/googletest/googlemock
-# Build directory: C:/Users/liuru/OneDrive/Research/NewProject/OpenRecon/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")

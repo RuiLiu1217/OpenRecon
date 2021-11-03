dc9afaa84316aca3cc4b4667eec41e1057abff21
Rui Liu <liurui1217@gmail.com>
2021-11-02 23:22:12 -0400

    A new start

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/liuru/Dropbox/MyWebsite/OpenRecon
-# Build directory: C:/Users/liuru/Dropbox/MyWebsite/OpenRecon/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/liuru/Dropbox/MyWebsite/OpenRecon/googletest/googlemock
-# Build directory: C:/Users/liuru/Dropbox/MyWebsite/OpenRecon/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
